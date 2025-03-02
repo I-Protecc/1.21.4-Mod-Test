463b3656d741ca637ecf5c776472c6422095cb1d
I-Protecc <ryanbaumeister@googlemail.com>
2025-03-02 17:35:49 +0100

    Initial commit

---
+++ src/client/java/github/iprotecc/infectionmod/InfectionModTestDataGenerator.java
@@ -0,0 +1,11 @@
+package github.iprotecc.infectionmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class InfectionModTestDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
